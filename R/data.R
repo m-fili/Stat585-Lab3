cd0d6d76a46831c3daa1cdbf8cc4acefc506154c
m-fili <mfili@iastate.edu>
2022-03-22 23:23:12 -0500

    Creating repo, adding folders, data, functions, documentations.

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Shape file of for Iran (level 0)
+#'
+#' A dataset with geographic information about the polygons of Iran which is created by using read_sf("gadm36_IRN_0.shp") file form
+#' https://gadm.org/
+#' @source \url{https://gadm.org/}
+"Iran"
